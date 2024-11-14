12cc2e0b4cfa92dd7617d64a2c558be25344741c
Ahmed Ibrahim Amin Elrifaiy <rifaiyahmed@gmail.com>
2024-11-14 07:22:25 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -52,3 +52,18 @@ class Document(models.Model):
             ("can_edit", "Can edit documents"),
             ("can_delete", "Can delete documents"),
         ]
+
+# models.py
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    summary = models.TextField()
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
