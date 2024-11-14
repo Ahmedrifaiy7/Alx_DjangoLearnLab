8be450924de99fc17102497bbc8ed6bd71d2d868
Ahmed Ibrahim Amin Elrifaiy <rifaiyahmed@gmail.com>
2024-11-14 07:24:46 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,3 +5,7 @@ def get_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     # Securely fetches book without direct SQL query
     return render(request, 'bookshelf/book_detail.html', {'book': book})
+
+def book_list(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'bookshelf/book_list.html', {'books': books})
