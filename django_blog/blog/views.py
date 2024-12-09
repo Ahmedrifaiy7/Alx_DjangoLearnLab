b6413c6c13469ab18514f8574f2fa3a5fa7b0ca1
Ahmed Ibrahim Amin Elrifaiy <rifaiyahmed@gmail.com>
2024-12-10 00:51:15 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -145,3 +145,19 @@ def search_posts(request):
     else:
         posts = Post.objects.all()  # Default to all posts if no query
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug']
+        return Post.objects.filter(tags__slug=tag_slug)  # Filter posts by tag slug
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_slug = self.kwargs['tag_slug']
+        tag = Tag.objects.get(slug=tag_slug)  # Get tag by slug
+        context['tag'] = tag
+        return context
