c87fc80637ed29896a4e1b77fa07f2bd10ddc845
Ahmed Ibrahim Amin Elrifaiy <rifaiyahmed@gmail.com>
2024-12-15 23:47:46 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,14 +1,22 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    # This is a ManyToMany relationship with itself to represent followers and following
+    following = models.ManyToManyField(
+        'self', 
+        symmetrical=False, 
+        related_name='followers', 
+        through='UserFollow', 
+        blank=True
+    )
 
-    def __str__(self):
-        return self.username
+class UserFollow(models.Model):
+    # This model stores the relationship between the follower and the followed user
+    follower = models.ForeignKey(CustomUser, related_name='following_set', on_delete=models.CASCADE)
+    followed = models.ForeignKey(CustomUser, related_name='followers_set', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-class User(AbstractUser):
-    following = models.ManyToManyField('self', symmetrical=False, blank=True)
+    class Meta:
+        # Ensure that a user can only follow another user once
+        unique_together = ('follower', 'followed')
